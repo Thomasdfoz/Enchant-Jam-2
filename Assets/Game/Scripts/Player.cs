c425ed5428f16a6a4bd8bf508e13cf8ca4c794f1
Thomasdfoz <thomas_sp18@hotmail.com>
2024-04-07 00:23:56 -0300

    update moviment and collider

--- Assets/Game/Scripts/Player.cs
+++ Assets/Game/Scripts/Player.cs
@@ -1,30 +1,48 @@
 using Cinemachine;
+using System.Threading;
 using UnityEngine;
+using static UnityEngine.Rendering.DebugUI;
 
 namespace Horror.Player
 {
     public class Player : MonoBehaviour
     {
+        [Header("Set Configs")]
         [SerializeField] private CinemachineVirtualCamera vCam3;
         [SerializeField] private CinemachineVirtualCamera vCam1;
         [SerializeField] private CharacterController character;
+        [SerializeField] private LayerMask groundMask;
+        [SerializeField] private float gravity = -1;
+
+        [Header("Player Stats")]
+        [SerializeField] private int maxLife;
+        [SerializeField] private int magazineAmmunition;
+        [SerializeField] private int initAmmunition;
         [SerializeField] private float moveSpeedFoward;
         [SerializeField] private float moveSpeedBack;
         [SerializeField] private float moveSpeedSides;
         [SerializeField] private float groundOffset;
-        [SerializeField] private LayerMask groundMask;
-        [SerializeField] private float gravity = -9.81f;
+        [SerializeField] private Vector2 mouseYSensitivity;
         [SerializeField] private float sensitivityMouseX = 500f;
         [SerializeField] private float sensitivityMouseY = 5f;
-        [SerializeField] private Vector2 mouseYSensitivity;
-        [SerializeField] private Animator animator;
 
+
+
+
+        [Header("Player Parts")]
+        [SerializeField] private GameObject[] partsDesativar;
 
         private CinemachineComposer camComposer;
+        private Animator animator;
         private float hzInput, vInput;
+        private int currentLife;
+        private bool die;
         private Vector3 dir;
         private Vector3 spherePos;
         private Vector3 velocity;
+        private bool value;
+        private bool isFirstCam;
+        float currentGravity;
 
         void Start()
         {
@@ -32,21 +50,70 @@ namespace Horror.Player
             animator = transform.GetChild(0).GetComponent<Animator>();
             Cursor.lockState = CursorLockMode.Locked;
             camComposer = vCam3.GetCinemachineComponent<CinemachineComposer>();
+            currentLife = maxLife;
+            currentGravity = gravity;
 
         }
 
         void Update()
         {
+            if (die) return;
+
             GetDirectionAndMove();
             Gravity();
+            SetRotation();
+            SetAnimations();
+        }
 
+        public void TakeDamage(int Damage)
+        {
+            currentLife -= Damage;
 
-            float mouseX = Input.GetAxis("Mouse X");
-            float mouseY = Input.GetAxis("Mouse Y");
+            if (currentLife < 0)
+            {
+                die = true;
+                currentLife = 0;
+            }
 
+        }
 
-            camComposer.m_ScreenY = Mathf.Clamp(camComposer.m_ScreenY + mouseY * sensitivityMouseY * Time.deltaTime, mouseYSensitivity.x, mouseYSensitivity.y);
-            transform.Rotate(Vector3.up, mouseX * sensitivityMouseX * Time.deltaTime, Space.World);
+        #region Private Methods
+        private void ChangeCam(bool firstCam)
+        {
+            isFirstCam = firstCam;
+
+            if (firstCam)
+            {
+                vCam3.gameObject.SetActive(false);
+                vCam1.gameObject.SetActive(true);
+
+                value = false;
+                Invoke(nameof(PartsEnable), 1f);
+            }
+            else
+            {
+                vCam3.gameObject.SetActive(true);
+                vCam1.gameObject.SetActive(false);
+                value = true;
+                Invoke(nameof(PartsEnable), 0.7f);
+            }
+
+        }
+
+        private void PartsEnable()
+        {
+            for (int i = 0; i < partsDesativar.Length; i++)
+            {
+                partsDesativar[i].gameObject.SetActive(value);
+            }
+        }
+
+        private void FireShot()
+        {
+
+        }
+        private void SetAnimations()
+        {
 
             animator.SetFloat("hInput", hzInput);
             animator.SetFloat("vInput", vInput);
@@ -61,6 +128,14 @@ namespace Horror.Player
                 animator.SetTrigger("reload");
             }
 
+        }
+
+        private void SetRotation()
+        {
+            float mouseX = Input.GetAxis("Mouse X");
+            float mouseY = Input.GetAxis("Mouse Y");
+            camComposer.m_ScreenY = Mathf.Clamp(camComposer.m_ScreenY + mouseY * sensitivityMouseY * Time.deltaTime, mouseYSensitivity.x, mouseYSensitivity.y);
+            transform.Rotate(Vector3.up, mouseX * sensitivityMouseX * Time.deltaTime, Space.World);
         }
 
         private void GetDirectionAndMove()
@@ -95,8 +170,12 @@ namespace Horror.Player
 
         private void Gravity()
         {
-            if (!IsGround()) velocity.y += gravity * Time.deltaTime;
-            else if (velocity.y < 0) velocity.y = -2;
+            if (!IsGround()) velocity.y += currentGravity * Time.deltaTime;
+            else if(velocity.y < 0) 
+            {
+                currentGravity = gravity;
+                velocity.y = -2;
+            }
 
             character.Move(velocity * Time.deltaTime);
         }
@@ -107,8 +186,25 @@ namespace Horror.Player
             Gizmos.DrawWireSphere(spherePos, character.radius - 0.05f);
         }
 
+        #endregion
 
+        private void OnTriggerStay(Collider other)
+        {
+            if (other.CompareTag("FirstCam"))
+            {
+                if (isFirstCam) return;
+                
+                ChangeCam(true);
+            }
+        }
 
-        
+        private void OnTriggerExit(Collider other)
+        {
+            if (other.CompareTag("FirstCam"))
+            {
+                ChangeCam(false);
+            }
+        }
+
     }
 }
